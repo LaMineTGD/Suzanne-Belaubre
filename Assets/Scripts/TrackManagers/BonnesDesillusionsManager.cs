7038f046e60484621c190e1a288ce0de2827129e
Benjamin Landes <90406524+BnLds@users.noreply.github.com>
2023-09-02 11:58:44 +0200

    Transition BonnesDes to AliceAlice

--- Assets/Scripts/TrackManagers/BonnesDesillusionsManager.cs
+++ Assets/Scripts/TrackManagers/BonnesDesillusionsManager.cs
@@ -1,8 +1,21 @@
+using UnityEngine;
+
 public class BonnesDesillusionsManager : ITrackManager
 {
+    [SerializeField] private Transform _background;
     protected override void Start()
     {
         base.Start();
         base.ApplyDefaultEffects();
+    }
+
+    public void RemoveBackground()
+    {
+        _background.Rotate(90f, 0f, 0f);
+    }
+
+    public void OnEnd()
+    {
+        Transition();
     }
 }
