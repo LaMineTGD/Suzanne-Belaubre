fe0335f26e80454b7d257bd81a7484cd72b933eb
YNicolas Decoster <ogadaki@mailbox.org>
2023-09-03 22:58:40 +0200

    [BD] Fix balls going outside vertical plane

--- Assets/Scenes/Tracks/BonnesDesillusions/StartForce.cs
+++ Assets/Scenes/Tracks/BonnesDesillusions/StartForce.cs
@@ -4,10 +4,25 @@ public class StartForce : MonoBehaviour
 {
     public Vector3 startForce;
 
-    void Start()
-    {
+    void Start() {
         Rigidbody rigidbody = GetComponent<Rigidbody>();
         rigidbody.AddForce(startForce, ForceMode.Impulse);
         Physics.gravity = new Vector3(0, -1.0F, 0);
+    }
+
+    void Update() {
+        KeepMovingInVerticalPlane();
+    }
+
+    void KeepMovingInVerticalPlane () {
+        // There was an unexpected behavior that makes some balls boucning
+        // outside of the vertical plane. We don't know why, so we implement
+        // this hack, that always keeps the velocity in the vertical plane.
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        if (rigidbody.velocity.z != 0) {
+            Vector3 newVelocity = rigidbody.velocity;
+            newVelocity.z = 0;
+            rigidbody.velocity = newVelocity;
+        }
     }
 }
