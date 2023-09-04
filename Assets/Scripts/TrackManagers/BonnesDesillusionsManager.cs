64d9a06b35d3c88b76e4e24689b042b79c800cb3
Benjamin Landes <90406524+BnLds@users.noreply.github.com>
2023-09-04 14:21:34 +0200

    [BD] Add transition OSC messages

--- Assets/Scripts/TrackManagers/BonnesDesillusionsManager.cs
+++ Assets/Scripts/TrackManagers/BonnesDesillusionsManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using extOSC;
 
 public class BonnesDesillusionsManager : ITrackManager
 {
@@ -11,14 +12,32 @@ public class BonnesDesillusionsManager : ITrackManager
         //Change the sky color
         var currentTrackData = ShowManager.m_Instance.GetCurrentTrack();
         SetSkyColor(currentTrackData._MainColorList[0], currentTrackData._MainColorList[1], currentTrackData._MainColorList[2]);
+
+        generateOSCReceveier();
+    }
+
+    private void generateOSCReceveier()
+    {
+        ShowManager.m_Instance.OSCReceiver.Bind("/remove_background", RemoveBackground);
+        ShowManager.m_Instance.OSCReceiver.Bind("/End", OnEnd);
     }
 
     public void RemoveBackground()
+    {
+        RemoveBackground(null);
+    }
+
+    public void RemoveBackground(OSCMessage message)
     {
         _background.Rotate(90f, 0f, 0f);
     }
 
     public void OnEnd()
+    {
+        OnEnd(null);
+    }
+
+    public void OnEnd(OSCMessage message)
     {
         Transition();
     }
