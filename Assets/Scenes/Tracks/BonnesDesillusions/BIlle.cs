618dbb78367e2063146ed07ce45fd521a9938951
YNicolas Decoster <ogadaki@mailbox.org>
2023-09-04 02:29:13 +0200

    [BD] Implements orbit phase + OSC messaging

--- Assets/Scenes/Tracks/BonnesDesillusions/BIlle.cs
+++ Assets/Scenes/Tracks/BonnesDesillusions/BIlle.cs
@@ -1,25 +1,105 @@
 using UnityEngine;
+using extOSC;
+
 
 public class StartForce : MonoBehaviour
 {
     public Vector3 startForce;
     public float rotationSpeed = 0.01F;
+    public float orbitSpeed = 15F;
+    [SerializeField] GameObject center;
+
+
+    Vector3 positionBeforeMovingToOrbit;
+    Vector3 orbitStartPosition;
+    float timeToReachOrbit;
+    float movingToOrbitProgress;
+    bool movingToOrbit = false;
+
 
     void Start() {
         Rigidbody rigidbody = GetComponent<Rigidbody>();
         rigidbody.AddForce(startForce, ForceMode.Impulse);
         Physics.gravity = new Vector3(0, -1.0F, 0);
+        generateOSCReceveier();
+    }
+
+    private void generateOSCReceveier()
+    {
+        ShowManager.m_Instance.OSCReceiver.Bind("/start_orbit", StartOrbit);
+        ShowManager.m_Instance.OSCReceiver.Bind("/start_bouncing", StartBouncing);
+    }
+
+    public void StartOrbit(OSCMessage message)
+    {
+        StartOrbit();
+    }
+
+    public void StartBouncing(OSCMessage message)
+    {
+        StartBouncing();
+    }
+
+    public void StartBouncing()
+    {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.isKinematic = false;
+        rigidbody.AddForce(startForce, ForceMode.Impulse);
+    }
+
+    public Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Vector3 angles) {
+        return Quaternion.Euler(angles) * (point - pivot) + pivot;
+    }
+
+    public void StartOrbit()
+    {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.isKinematic = true;
+
+        positionBeforeMovingToOrbit = transform.position;
+        orbitStartPosition = center.transform.position;
+        orbitStartPosition.y +=
+            5F
+            + 2F * Mathf.PerlinNoise1D(transform.position.x + transform.position.y);
+        var angle = Random.Range(0F, 360F);
+        orbitStartPosition = RotatePointAroundPivot(
+            orbitStartPosition,
+            center.transform.position,
+            new Vector3(0, 0, angle)
+        );
+        timeToReachOrbit = 1.5F;
+        movingToOrbitProgress = 0F;
+        movingToOrbit = true;
+    }
+
+    void MaybeMoveToOrbit() {
+        if (movingToOrbit) {
+            movingToOrbitProgress += Time.deltaTime/timeToReachOrbit;
+            transform.position = Vector3.Lerp(
+                positionBeforeMovingToOrbit,
+                orbitStartPosition,
+                movingToOrbitProgress
+            );
+            if (movingToOrbitProgress > 1) {
+                movingToOrbit = false;
+            }
+        }
     }
 
     void Update() {
         KeepMovingInVerticalPlane();
         Rotate();
+        // We choose to keep orbit movements even for the bouncing phase. This
+        // will add some non linear moving to the bouncing movements.
+        Orbit();
+        MaybeMoveToOrbit();
     }
 
     void KeepMovingInVerticalPlane () {
-        // There was an unexpected behavior that makes some balls boucning
-        // outside of the vertical plane. We don't know why, so we implement
-        // this hack, that always keeps the velocity in the vertical plane.
+        // There was an unexpected behavior that makes some balls bouncing
+        // outside of the vertical plane. We don't know why. So we implement
+        // this hack, that always keeps the velocity in the vertical plane (i.e.
+        // forcing velocity.z to 0).
         Rigidbody rigidbody = GetComponent<Rigidbody>();
         if (rigidbody.velocity.z != 0) {
             Vector3 newVelocity = rigidbody.velocity;
@@ -33,5 +113,13 @@ public class StartForce : MonoBehaviour
         var perlinY = 0.5F * (this.transform.position.x + this.transform.position.y) * rotationSpeed;
         var angle = 50.0F * (Mathf.PerlinNoise(perlinX, perlinY) - 0.5F);
         transform.Rotate(new Vector3(0, 0, angle));
+    }
+
+    void Orbit () {
+        this.transform.RotateAround(
+            center.transform.position,
+            Vector3.forward,
+            orbitSpeed * Time.deltaTime
+        );
     }
 }
