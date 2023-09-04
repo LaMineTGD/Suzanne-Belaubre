c140ce0b7d30fd054cc92fb7de423f127bbba991
YNicolas Decoster <ogadaki@mailbox.org>
2023-09-04 15:59:19 +0200

    [BD] Add pixelize background effect

--- Assets/Scenes/Tracks/BonnesDesillusions/Background.cs
+++ Assets/Scenes/Tracks/BonnesDesillusions/Background.cs
@@ -13,6 +13,7 @@ public class Background : MonoBehaviour
     {
         ShowManager.m_Instance.OSCReceiver.Bind("/BD/start_couplet", StartCouplet);
         ShowManager.m_Instance.OSCReceiver.Bind("/BD/start_refrain", StartRefrain);
+        ShowManager.m_Instance.OSCReceiver.Bind("/BD/start_fire", StartFire);
     }
 
     public void StartCouplet(OSCMessage message)
@@ -25,7 +26,9 @@ public class Background : MonoBehaviour
     {
         var renderer = GetComponent<Renderer>();
         var material = renderer.material;
+        material.SetFloat("_Speed", 0.1F);
         material.SetFloat("_FuzzyGrain", 80.0F);
+        material.SetFloat("_PixelizeFlag", 0);
 
         var shader = material.shader;
         material.SetKeyword(
@@ -48,7 +51,9 @@ public class Background : MonoBehaviour
     {
         var renderer = GetComponent<Renderer>();
         var material = renderer.material;
+        material.SetFloat("_Speed", 0.1F);
         material.SetFloat("_FuzzyGrain", 800.0F);
+        material.SetFloat("_PixelizeFlag", 0);
 
         var shader = material.shader;
         material.SetKeyword(
@@ -61,6 +66,30 @@ public class Background : MonoBehaviour
         );
     }
 
+    public void StartFire(OSCMessage message)
+    {
+        Debug.Log("StartFire");
+        StartFire();
+    }
+
+    public void StartFire()
+    {
+        var renderer = GetComponent<Renderer>();
+        var material = renderer.material;
+        material.SetFloat("_Speed", 0.9F);
+        material.SetFloat("_FuzzyGrain", 800.0F);
+        material.SetFloat("_PixelizeFlag", 1);
+
+        var shader = material.shader;
+        material.SetKeyword(
+            new LocalKeyword(shader, "_PALETTE_REFRAIN"),
+            true
+        );
+        material.SetKeyword(
+            new LocalKeyword(shader, "_PALETTE_COUPLET"),
+            false
+        );
+    }
 
     void Update() {
     }
