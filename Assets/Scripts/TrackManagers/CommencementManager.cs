16e9cdb7b192697745686ccb5d4670bb4af7d605
Gnurf <gnurf.de.la.grande.cahute@gmail.com>
2023-09-04 19:31:32 +0200

    Commencement interactif + vfx alarmes

--- Assets/Scripts/TrackManagers/CommencementManager.cs
+++ Assets/Scripts/TrackManagers/CommencementManager.cs
@@ -1,7 +1,13 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
 using extOSC;
 
 public class CommencementManager : ITrackManager
 {
+    public GameObject m_VFX;
+    public float m_Speed;
+    Vector3 m_BaseVFXPosition;
+
     protected override void Start()
     {
         base.Start();
@@ -9,7 +15,8 @@ public class CommencementManager : ITrackManager
         //Change the sky color
         var currentTrackData = ShowManager.m_Instance.GetCurrentTrack();
         SetSkyColor(currentTrackData._MainColorList[0], currentTrackData._MainColorList[1], currentTrackData._MainColorList[2]);
-    
+        m_BaseVFXPosition = m_VFX.transform.position;
+
         generateOSCReceveier();
     }
 
@@ -26,5 +33,12 @@ public class CommencementManager : ITrackManager
     public void OnTransition(OSCMessage message)
     {
         Transition();
+    }
+
+    void OnTremble(InputValue _Value)
+    {
+        Debug.Log(m_BaseVFXPosition.z);
+        Vector3 _TempPos = new Vector3(m_BaseVFXPosition.x + _Value.Get<Vector2>().x * m_Speed, m_BaseVFXPosition.y, m_BaseVFXPosition.z + _Value.Get<Vector2>().y * m_Speed);
+        m_VFX.transform.position = _TempPos;
     }
 }
