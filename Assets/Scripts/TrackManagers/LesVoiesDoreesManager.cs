59fc35d2f5b8f3d2d720995b3b1cfeba6debb5cd
Benjamin Landes <90406524+BnLds@users.noreply.github.com>
2023-09-05 16:10:39 +0200

    Change duration transi fin LVD

--- Assets/Scripts/TrackManagers/LesVoiesDoreesManager.cs
+++ Assets/Scripts/TrackManagers/LesVoiesDoreesManager.cs
@@ -57,7 +57,7 @@ public class LesVoiesDoreesManager : TrackTailorMadeManager
     private IEnumerator ToBlackCoroutine()
     {
         float elapsedTime = 0f;
-        float duration = 3f;
+        float duration = 2f;
 
         while(elapsedTime < duration)
         {
