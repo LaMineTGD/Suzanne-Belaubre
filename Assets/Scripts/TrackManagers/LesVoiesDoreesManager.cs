a04e30c4b51b94999fb1aae25520de141575bf9a
Benjamin Landes <90406524+BnLds@users.noreply.github.com>
2023-09-05 12:06:56 +0200

    LVD Transition to black

--- Assets/Scripts/TrackManagers/LesVoiesDoreesManager.cs
+++ Assets/Scripts/TrackManagers/LesVoiesDoreesManager.cs
@@ -1,8 +1,11 @@
+using System.Collections;
 using extOSC;
-using Utils;
+using UnityEngine;
 
 public class LesVoiesDoreesManager : TrackTailorMadeManager
 {
+    [SerializeField] private Material _backgroundMaterial;
+
     protected override void Start()
     {
         base.Start();
@@ -10,14 +13,24 @@ public class LesVoiesDoreesManager : TrackTailorMadeManager
          //Change the sky color
         var currentTrackData = ShowManager.m_Instance.GetCurrentTrack();
         SetSkyColor(currentTrackData._MainColorList[0], currentTrackData._MainColorList[1], currentTrackData._MainColorList[2]);
-        //BoostTones();
         generateOSCReceveier();
-
+        _backgroundMaterial.SetFloat("_ColorMultiplier", 1f);
     }
 
     private void generateOSCReceveier()
     {
-        ShowManager.m_Instance.OSCReceiver.Bind("/End", OnEnd);
+        ShowManager.m_Instance.OSCReceiver.Bind("/ToBlack", ToBlack);
+        // ShowManager.m_Instance.OSCReceiver.Bind("/End", OnEnd);
+    }
+
+    public void ToBlack()
+    {
+        ToBlack(null);
+    }
+
+    public void ToBlack(OSCMessage message)
+    {
+        StartCoroutine(ToBlackCoroutine());
     }
 
     public void OnEnd()
@@ -28,5 +41,21 @@ public class LesVoiesDoreesManager : TrackTailorMadeManager
     public void OnEnd(OSCMessage message)
     {
         Transition();
+    }
+
+    private IEnumerator ToBlackCoroutine()
+    {
+        float elapsedTime = 0f;
+        float duration = 3f;
+
+        while(elapsedTime < duration)
+        {
+            _backgroundMaterial.SetFloat("_ColorMultiplier", Mathf.Lerp(1f, 0f, elapsedTime / duration));
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
+        yield return null;
+        OnEnd();
     }
 }
